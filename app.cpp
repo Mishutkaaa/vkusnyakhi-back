91ece9fa5cc5dfe972c20a9e4ea62e3eb14d1e6b
Mishnevsky <zzMishutkazz@yandex.ru>
2025-10-17 16:52:25 +0500

    output drinks and food

--- app.cpp
+++ app.cpp
@@ -4,32 +4,17 @@
 int main() {
     crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/")([](){
-        return "Всё ок";
-    });
-
     CROW_ROUTE(app, "/drinks")([](){
-        crow::json::wvalue response;
-        // METHOD GET
-        // РУЧКА ДЛЯ ПОЛУЧАНИЯ НАПИТКОВ, ОТДАЁМ ОТВЕТ В ВИДЕ 
-        // {
-        // "id":1, 
-        // "name":"name",
-        //  "image":"image"
-        // }
-        return response;
+crow::json::wvalue x({{"id", "row[0]"}}); //Select from drinks where row = 1
+    x["name"] = "row[1]";
+    x["image"] = "row[2]";
+    return x;
     });
-
     CROW_ROUTE(app, "/food")([](){
-        crow::json::wvalue response;
-        // METHOD GET
-        // РУЧКА ДЛЯ ПОЛУЧАНИЯ ЕДЫ, ОТДАЁМ ОТВЕТ В ВИДЕ 
-        // {
-        // "id":1, 
-        // "name":"name",
-        //  "image":"image"
-        // }
-        return response;
+      crow::json::wvalue x({{"id", "row[0]"}});
+    x["name"] = "row[1]";
+    x["image"] = "row[2]";
+    return x;
     });
 
     CROW_ROUTE(app, "/categories")([](){
@@ -58,7 +43,7 @@ int main() {
     std::cout << "=== C++ Backend Server ===" << std::endl;
     std::cout << "Server running on: http://localhost:8080" << std::endl;
     std::cout << "Available endpoints:" << std::endl;
-    std::cout << "  GET /" << std::endl;
+
     std::cout << "  GET /drinks" << std::endl;
     std::cout << "  GET /food" << std::endl;
     std::cout << "  GET /newProduct" << std::endl;
