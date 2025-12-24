cebba618c8e29dac6d57d7e74694e4cdc11fd1f8
Mishutka <zzMishutkazz@yandex.ru>
2025-12-24 12:14:04 +0500

    update food endpoint

--- main.go
+++ main.go
@@ -49,12 +49,11 @@ func main() {
 			where = append(where, fmt.Sprintf(" categories @> array [%s]", categories))
 		}
 		if brand != "" {
-			where = append(where, fmt.Sprintf( "brand = %s", brand))
+			where = append(where, fmt.Sprintf("brand = %s", brand))
 		}
 		if len(where) != 0 {
 			query += " where " + strings.Join(where, "and")
 		}
-		log.Println(query)
 		rows, err := db.Query(query)
 		if err != nil {
 			log.Println(err)
@@ -75,12 +74,27 @@ func main() {
 
 	http.HandleFunc("/food", func(w http.ResponseWriter, r *http.Request) {
 		var foods []models.Food
+		var query string = "select id, name, image from food"
+		var where []string
 
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 
-		rows, err := db.Query("select id, name, image from food")
+		categories := r.URL.Query().Get("categories")
+		brand := r.URL.Query().Get("brand")
+
+		if categories != "" {
+			where = append(where, fmt.Sprintf(" categories @> array [%s]", categories))
+		}
+		if brand != "" {
+			where = append(where, fmt.Sprintf("brand = %s", brand))
+		}
+		if len(where) != 0 {
+			query += " where " + strings.Join(where, "and")
+		}
+
+		rows, err := db.Query(query)
 		if err != nil {
 			log.Println(err)
 			http.Error(w, "cannot get food", http.StatusInternalServerError)
