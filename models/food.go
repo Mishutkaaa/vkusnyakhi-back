5ce2edfabbf5ca66e06d2a65e86ed857da635224
Mishutka <zzMishutkazz@yandex.ru>
2025-12-24 13:14:48 +0500

    update models

--- models/food.go
+++ models/food.go
@@ -1,7 +1,9 @@
 package models
 
 type Food struct {
-	ID    int     `json:"id"`
-	Name  string  `json:"name"`
-	Image *string `json:"image,omitempty"`
+	ID         int     `json:"id"`
+	Name       string  `json:"name"`
+	Image      *string `json:"image,omitempty"`
+	Categories *[]int  `json:"categories,omitempty"`
+	Brand      *int    `json:"brand,omitempty"`
 }
