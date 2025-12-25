e87ead4b1938636a84abfc443a1f3519684d4962
Mishutka <zzMishutkazz@yandex.ru>
2025-12-25 13:01:05 +0500

    update models

--- models/drinks.go
+++ models/drinks.go
@@ -1,9 +1,11 @@
 package models
 
+import "github.com/lib/pq"
+
 type Drinks struct {
-	ID         int     `json:"id"`
-	Name       string  `json:"name"`
-	Image      *string `json:"image,omitempty"`
-	Categories *[]int  `json:"categories,omitempty"`
-	Brand      *int    `json:"brand,omitempty"`
+	ID         int           `json:"id"`
+	Name       string        `json:"name"`
+	Image      *string       `json:"image,omitempty"`
+	Categories pq.Int32Array `json:"categories" db:"categories"`
+	Brand      *int          `json:"brand,omitempty"`
 }
--- models/food.go
+++ models/food.go
@@ -1,9 +1,11 @@
 package models
 
+import "github.com/lib/pq"
+
 type Food struct {
-	ID         int     `json:"id"`
-	Name       string  `json:"name"`
-	Image      *string `json:"image,omitempty"`
-	Categories *[]int  `json:"categories,omitempty"`
-	Brand      *int    `json:"brand,omitempty"`
+	ID         int           `json:"id"`
+	Name       string        `json:"name"`
+	Image      *string       `json:"image,omitempty"`
+	Categories pq.Int32Array `json:"categories" db:"categories"`
+	Brand      *int          `json:"brand,omitempty"`
 }
